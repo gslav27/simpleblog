f476c628b7a5cd74afc3a9da6c23c147c8c3e869
gslav27 <stepanko182@gmail.com>
2019-01-21 00:33:22 +0200

    delete unused comments

--- src_frontEnd/src/reducers/index.js
+++ src_frontEnd/src/reducers/index.js
@@ -1,16 +1,10 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
-// import searchReducer from './searchReducer';
-// import imgViewerReduser from './imgViewerReducer';
-// import appAddsReducer from './appAddsReducer';
 import postsReducer from './postsReducer';
 
 
 export default combineReducers({
-  // search: searchReducer,
-  // imgViewer: imgViewerReduser,
-  // appAdds: appAddsReducer,
   posts: postsReducer,
   routing: routerReducer,
 });
