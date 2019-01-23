1763c0a4d2360da53e4b79a11f0dca283c58e9e3
gslav27 <stepanko182@gmail.com>
2019-01-23 13:25:46 +0200

    add Babel plugins for async_await and update all Promises

--- src_frontEnd/src/operations/postsOperations.js
+++ src_frontEnd/src/operations/postsOperations.js
@@ -13,6 +13,7 @@ const getFetchObj = (method, bodyData) => {
 
 const getFetchURL = (type = 'posts', postId = '', comments = false) => {
   const apiURL = 'http://localhost:3001';
+  // const apiURL = 'https://my-json-server.typicode.com/gslav27/fakeJsonServer/';
   const collectionUrl = type;
   return `${apiURL}/${collectionUrl}${postId ? `/${postId}${comments ? '?_embed=comments' : ''}` : ''}`;
 };
@@ -36,62 +37,47 @@ const getLatestPosts = posts => posts
   .slice(0, 10);
 
 
-export const fetchLatestPosts = () => (dispatch) => {
+export const fetchLatestPosts = () => async (dispatch) => {
   dispatch(postsActions.waitResponse('1'));
-  fetch(getFetchURL())
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-    })
-    .then((posts) => {
-      dispatch(postsActions.loadFetchedPosts(getLatestPosts(posts)));
-    })
-    .catch((error) => {
-      console.log('There has been a problem with fetching latest posts: ', error.message);
-      dispatch(postsActions.waitResponse(1, false));
-    });
+  try {
+    const result = await fetch(getFetchURL());
+    const resultJSON = await result.json();
+    dispatch(postsActions.loadFetchedPosts(getLatestPosts(resultJSON)));
+  } catch (err) {
+    console.log('There has been a problem with fetching latest posts: ', err.message);
+    dispatch(postsActions.waitResponse(1, false));
+  }
 };
 
 
-export const fetchCurrentPostData = postId => (dispatch) => {
+export const fetchCurrentPostData = postId => async (dispatch) => {
   dispatch(postsActions.waitResponse('2'));
-  fetch(getFetchURL(undefined, postId))
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-    })
-    .then((response) => {
-      dispatch(postsActions.loadCurrentPostData(response));
-      dispatch(loadCurrentPostComments(postId));
-    })
-    .catch((error) => {
-      console.log('There has been a problem with fetching current post data: ', error.message);
-      dispatch(postsActions.waitResponse(2, false));
-    });
+  try {
+    const result = await fetch(getFetchURL(undefined, postId));
+    const resultJSON = await result.json();
+    dispatch(postsActions.loadCurrentPostData(resultJSON));
+    dispatch(loadCurrentPostComments(postId));
+  } catch (err) {
+    console.log('There has been a problem with fetching current post data: ', err.message);
+    dispatch(postsActions.waitResponse(2, false));
+  }
 };
 
 
-export const loadCurrentPostComments = postId => (dispatch) => {
+export const loadCurrentPostComments = postId => async (dispatch) => {
   dispatch(postsActions.waitResponse('3'));
-  fetch(getFetchURL(undefined, postId, true))
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-    })
-    .then((response) => {
-      dispatch(postsActions.loadCurrentPostComments(response.comments));
-    })
-    .catch((error) => {
-      console.log('There has been a problem with fetching current post data: ', error.message);
-      dispatch(postsActions.waitResponse(3, false));
-    });
+  try {
+    const result = await fetch(getFetchURL(undefined, postId, true));
+    const resultJSON = await result.json();
+    dispatch(postsActions.loadCurrentPostComments(resultJSON.comments));
+  } catch (err) {
+    console.log('There has been a problem with fetching current post data: ', err.message);
+    dispatch(postsActions.waitResponse(3, false));
+  }
 };
 
 
-export const addComment = (body, author, postId) => (dispatch) => {
+export const addComment = (body, author, postId) => async (dispatch) => {
   const currentDate = new Date();
   const formattedDate = currentDate.toLocaleDateString();
   const newCommentData = {
@@ -100,14 +86,13 @@ export const addComment = (body, author, postId) => (dispatch) => {
     author,
     body,
   };
-  fetch(getFetchURL('comments'), getFetchObj('POST', newCommentData))
-    .then((response) => {
-      if (response.ok) {
-        dispatch(loadCurrentPostComments(postId));
-      }
-    })
-    .catch((error) => {
-      console.log('There has been a problem with adding new comment: ', error.message);
-      dispatch(postsActions.waitResponse(2, false));
-    });
+  try {
+    const result = await fetch(getFetchURL('comments'), getFetchObj('POST', newCommentData));
+    const resultJSON = await result.json();
+    console.log('response OK', resultJSON);
+    dispatch(loadCurrentPostComments(postId));
+  } catch (err) {
+    console.log('There has been a problem with adding new comment: ', err.message);
+    dispatch(postsActions.waitResponse(2, false));
+  }
 };
