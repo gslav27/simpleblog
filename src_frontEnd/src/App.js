07169b8a6e565b49e6a14080d0f7b1e30400814d
gslav27 <stepanko182@gmail.com>
2019-01-28 17:49:07 +0200

    fix App.js location and update root styles

--- src_frontEnd/src/App.js
+++ src_frontEnd/src/App.js
@@ -2,19 +2,35 @@ import React from 'react';
 
 import { Route, Switch } from 'react-router-dom';
 
+// import Spinner from './components/UI/Spinner';
 import LatestPosts from './containers/LatestPosts';
 import Post from './containers/Post';
-import HomeLink from './components/HomeLink';
+
+import Header from './containers/Header';
 import RootStyle from './App.styles';
+
+import { routes } from './constants';
+
+// const LazyLatestPosts = lazy(() => import(/* webpackChunkName: "LatestPosts" */'./containers/LatestPosts'));
+// const LazyPost = lazy(() => import(/* webpackChunkName: "Post" */ './containers/Post'));
+
+// // const LatestPosts = import(/* webpackChunkName: "LatestPosts" */'./containers/LatestPosts');
+// // const Post = import(/* webpackChunkName: "Post" */ './containers/Post');
+// // const LazyLatestPosts = lazy(() => LatestPosts);
+// // const LazyPost = lazy(() => Post);
 
 const App = () => (
   <React.Fragment>
     <RootStyle />
-    <HomeLink />
+    <Header />
+    {/* <Suspense fallback={<Spinner />}> */}
     <Switch>
-      <Route exact path='/' component={LatestPosts} />
-      <Route exact path='/posts/:postId' component={Post} />
+      <Route exact path={`${process.env.PUBLIC_URL}/`} component={LatestPosts} />
+      {/* <Route exact path='/' render={props => <LazyLatestPosts {...props} />} /> */}
+      <Route exact path={`${process.env.PUBLIC_URL}${routes.posts}/:postId`} component={Post} />
+      {/* <Route exact path='/posts/:postId' render={props => <LazyPost {...props} />} /> */}
     </Switch>
+    {/* </Suspense> */}
   </React.Fragment>
 );
 
--- src_frontEnd/src/App.styles.js
+++ src_frontEnd/src/App.styles.js
@@ -1,14 +1,17 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+  & * {
+    font-family: 'Merriweather', serif;
+  };
   #root {
     margin: 0 auto;
     padding: 0.5em;
     max-width: 1000px;
     font-size: 1.2em;
-    & * {
-      font-family: 'Merriweather', serif;
-    };
+    // & * {
+    //   font-family: 'Merriweather', serif;
+    // };
   };
   ${({ theme }) => theme.hightResolutionMediaMixin(
     `#root {
