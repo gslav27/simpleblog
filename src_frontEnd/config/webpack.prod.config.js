752da0b740ec0f7547ba07a414ed021c25f3c946
gslav27 <stepanko182@gmail.com>
2019-01-29 00:55:44 +0200

    fix prod config

--- src_frontEnd/config/webpack.prod.config.js
+++ src_frontEnd/config/webpack.prod.config.js
@@ -14,28 +14,28 @@ const prodConfiguration = {
       chunks: 'all',
       minChunks: 2,
     },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './public/index.html',
-        filename: './index.html',
-        inject: true,
-        minify: {
-          collapseWhitespace: true,
-          collapseInlineTagWhitespace: true,
-          minifyCSS: true,
-          minifyURLs: true,
-          minifyJS: true,
-          removeComments: true,
-          removeRedundantAttributes: true,
-        },
-      }),
-      new BundleAnalyzerPlugin(),
-    ],
-    output: {
-      filename: 'static/[name].js',
-      path: path.resolve(__dirname, '../dist/'),
-      publicPath: '',
-    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+      inject: true,
+      minify: {
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true,
+        minifyCSS: true,
+        minifyURLs: true,
+        minifyJS: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+      },
+    }),
+    new BundleAnalyzerPlugin(),
+  ],
+  output: {
+    filename: 'static/[name].js',
+    path: path.resolve(__dirname, '../dist/'),
+    publicPath: '',
   },
 };
 
