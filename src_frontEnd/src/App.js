282a61f0ee377366fe12556e35ea18cdad95bd1b
gslav27 <stepanko182@gmail.com>
2019-03-13 20:14:03 +0200

    temporary remove commented code

--- src_frontEnd/src/App.js
+++ src_frontEnd/src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-// import React, { Suspense, lazy } from 'react';
 
 import { Route, Switch } from 'react-router-dom';
 
 import { routes } from './utilities/constants/constants';
-// import Spinner from './components/UI/Spinner';
 import LatestPosts from './containers/LatestPosts';
 import Post from './containers/Post';
 
 import Header from './components/Header/HeaderLayout';
 import RootStyle from './App.styles';
 
-// import { routes } from './utilities/constants';
-
-// const LazyLatestPosts = lazy(() => import(/* webpackChunkName: "LatestPosts" */'./containers/LatestPosts'));
-// const LazyPost = lazy(() => import(/* webpackChunkName: "Post" */ './containers/Post'));
-
-// // const LatestPosts = import(/* webpackChunkName: "LatestPosts" */'./containers/LatestPosts');
-// // const Post = import(/* webpackChunkName: "Post" */ './containers/Post');
-// // const LazyLatestPosts = lazy(() => LatestPosts);
-// // const LazyPost = lazy(() => Post);
 
 const App = () => (
   <React.Fragment>
@@ -29,12 +18,6 @@ const App = () => (
       <Route exact path={`${process.env.PUBLIC_URL}/`} component={LatestPosts} />
       <Route exact path={`${process.env.PUBLIC_URL}${routes.posts}/:postId`} component={Post} />
     </Switch>
-    {/* <Suspense fallback={<Spinner />}>
-      <Switch>
-        <Route exact path={`${process.env.PUBLIC_URL}/`} render={props => <LazyLatestPosts {...props} />} />
-        <Route exact path={`${process.env.PUBLIC_URL}${routes.posts}/:postId`} render={props => <LazyPost {...props} />} />
-      </Switch>
-    </Suspense> */}
   </React.Fragment>
 );
 
